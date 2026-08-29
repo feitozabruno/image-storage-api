39d37221c21147877105e279f6b62ef7af9745f2
Bruno Feitoza <feitozabruno@hotmail.com>
2026-08-28 20:19:25 -0400

    mend

--- ImageStorage.Api/Program.cs
+++ ImageStorage.Api/Program.cs
@@ -1,5 +1,13 @@
+using ImageStorage.Api.Helpers;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddScoped<ImageOptimizerService>();
+builder.Services.AddHttpClient<VercelBlobService>();
+
 var app = builder.Build();
+app.MapControllers();
 
 app.MapGet("/", () => "Hello World!");
 
